425488cb9b4426405e1639e9fd96efd931df9e7c
YellowFlash1040 <andrejkovtunec@gmail.com>
2024-06-17 22:24:28 +0200

    I commented a couple of lines in the TestPage file, so that the site will deploy. Because there was an import that coudn't be done, because I included Slider2 compoent's folder to the .gitignore file. You can see the previous commit for more info

--- src/pages/TestPage/TestPage.tsx
+++ src/pages/TestPage/TestPage.tsx
@@ -1,13 +1,13 @@
 import s from './TestPage.module.css';
 
 // import Slider_Test from '../../components/Slider/Slider';
-import Slider2 from '../../components/Slider2/Slider2';
+// import Slider2 from '../../components/Slider2/Slider2';
 
 const TestPage = () => {
   return (
     <main className={s.container}>
       {/* <Slider_Test /> */}
-      <Slider2 />
+      {/* <Slider2 /> */}
     </main>
   );
 };
